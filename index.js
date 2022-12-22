87db8c89147c05152c5779a7ecdb4773b3eddc8a
Kobi Malka <kobim@onvego.com>
2022-12-23 00:11:30 +0200

    add logs

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const url =
   ":" +
   password +
   "@gelnailpolishmanagement.clpgail.mongodb.net/?retryWrites=true&w=majority";
-
+console.log(url);
 mongoose.set("strictQuery", true);
 mongoose.connect(url);
 
